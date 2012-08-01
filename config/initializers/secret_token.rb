8b88b60d16c143c902638e7136bda88696b1dc5c
Kendroid + NatashaTheRobot <apprentice+k+n@devbootcamp.com>
2012-08-01 10:49:16 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Nkcamp::Application.config.secret_token = 'bf4c057c3b59e57180285207cd13bdbafe14e49e9b43202f30ee70738182256411f626796efdc0b7523256bb8b7f4ad6881a0b3107c1b8b24de9bab335d65ce4'
